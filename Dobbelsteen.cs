6569c15f1ade880af382e617352ce800ce367540
Infernalis8 <107176370+Infernalis8@users.noreply.github.com>
2026-06-11 11:22:48 +0200

    Add edited property to Dobbelsteen class

--- Dobbelsteen.cs
+++ Dobbelsteen.cs
@@ -40,6 +40,7 @@ namespace Create_Dobbelsteen_Demo
         public string naam;
         public string vorm;
         public string materiaal;
+        public bool edited;
 
         //private static Random willekeurigewaarde = new Random();
         private Random willekeurigewaarde = new Random();
