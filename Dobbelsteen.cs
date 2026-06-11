92d8d3572272b34164e04ad6f71625d9fcc99737
Tijs Wishaupt <2508110wishaupt@zuyd.nl>
2026-06-11 11:20:37 +0200

    Dobbelsteen zijdes van 6 naar 8

--- Dobbelsteen.cs
+++ Dobbelsteen.cs
@@ -52,9 +52,9 @@ namespace Create_Dobbelsteen_Demo
             willekeurigewaarde = new Random(seed);
 
             naam = "Standaard dobbelsteen";
-            zijden = 6;
+            zijden = 8;
             kleur = "blauw";
-            waarden = new List<int> { 1, 2, 3, 4, 5, 6 };
+            waarden = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8};
             materiaal = "Plastic";
             vorm = "Kubus";
         }
